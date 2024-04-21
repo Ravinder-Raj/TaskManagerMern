209c980fecfc09b0a140feafa76628d86dac3dfa
Ravinder Raj <59919599+Ravinder-Raj@users.noreply.github.com>
2024-04-21 15:43:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json()); 
-app.use(cors()); 
+app.use(cors({
+    origin: ["https://gotaskmanager.netlify.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 
 mongoose
 .connect(process.env.MONGO_URI)
