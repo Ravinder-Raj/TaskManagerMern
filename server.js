795076950527c81a98fb174ce26ea8425d5f1661
Ravinder Raj <59919599+Ravinder-Raj@users.noreply.github.com>
2024-04-21 15:57:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config()
-const routes = require("./routes/TaskRoutes")
-
+const dotenv = require("dotenv");
 const cors = require("cors");
+const routes = require("./routes/TaskRoutes");
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-
-app.use(express.json()); 
+// Middleware
 app.use(cors({
     origin: ["https://gotaskmanager.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
+app.use(express.json());
 
+// MongoDB Connection
 mongoose
-.connect(process.env.MONGO_URI)
-.then(()=> console.log('Mongo db connected...'))
-.catch((err) => console.log(err));
-  
-//all routes
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+// Routes
 app.use("/api", routes);
-app.listen(PORT, () => console.log(`Listening at ${PORT}`));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start server
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
